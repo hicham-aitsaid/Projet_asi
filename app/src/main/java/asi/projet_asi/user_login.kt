841849395d5d45c0c62779a86caa6d90dbf07e27
hicham-aitsaid <hh_aitsaid@esi.dz>
2022-02-26 16:45:49 +0100

    2

---
+++ app/src/main/java/asi/projet_asi/user_login.kt
@@ -0,0 +1,11 @@
+package asi.projet_asi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class user_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_login)
+    }
+}
